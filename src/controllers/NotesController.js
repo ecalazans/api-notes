29b36c67e1760569579179bf1917b6a7067d3300
Erick D. Calazans <erickdcalazans@gmail.com>
2023-02-21 22:38:48 -0300

    :sparkles: new files: controllers and upload img

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -5,7 +5,7 @@ const knex = require("../database/knex");
 class NotesController {
   async create(request, response) {
     const { title, description, tags, links } = request.body;
-    const { user_id } = request.params;
+    const user_id = request.user.id;
 
     // Utilizando knex: Passando o obj para cadastro no BD
     const note_id = await knex("notes").insert({
@@ -66,13 +66,15 @@ class NotesController {
 
   //método responsável por listar as notas
   async index(request, response) {
-    const { title, user_id, tags } = request.query;
-
+    const { title, tags } = request.query;
+    
+    const user_id = request.user.id;
+    
     let notes;
-
+    
     if ( tags ) {
       const filterTags = tags.split(',').map(tag => tag.trim());
-
+      
       notes = await knex("tags")
       .select([
         "notes.id",
@@ -84,24 +86,23 @@ class NotesController {
       .whereIn("name", filterTags)
       .innerJoin("notes", "notes.id", "tags.note_id")
       .orderBy("notes.title");
-
+      
     } else {
       notes = await knex("notes")
       .where({ user_id })
       .whereLike("title",`%${title}%`)
       .orderBy("title");
     }
-
+    
     const userTags = await knex("tags").where({ user_id });
     const notesWithTags = notes.map(note => {
       const noteTags = userTags.filter(tag => tag.note_id === note.id);
-
+      
       return {
         ...note,
         tags: noteTags
       }
     })
-
 
     return response.json(notesWithTags);
   }
--- src/server.js
+++ src/server.js
@@ -2,14 +2,19 @@ require("express-async-errors")
 
 const migrationsRun = require("./database/sqlite/migrations") // por padão carrega o index.js
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload");
 
+const cors = require("cors"); // biblioteca importante para conectar o back-end com o front-end
 const express = require("express")
 const routes = require("./routes"); // por padão carrega o index.js
 
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json()); // configuração do uso de JSON no envio e leitura de dados nas ROTAS
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
